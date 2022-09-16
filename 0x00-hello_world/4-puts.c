51b50b2183b5958073ba816cbf0b6f2b413fea3e
Leckson Chigoneka <lecksonchigoneka@gmail.com>
2022-09-15 18:44:52 -0700

    QUestion 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main- print the string in the put function
+ * Descriptions : Using the main function
+ * this program will prints ' Programming is like building a multilingual puzzle'
+ * return o
+ */
+int main(void)
+{
+puts("\" Programming is like building a multilingual puzzle");
+return(0);
+}
