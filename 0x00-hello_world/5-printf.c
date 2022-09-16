4b5ef78b13014d12cd7538ee9335d5475be47b1c
Leckson Chigoneka <lecksonchigoneka@gmail.com>
2022-09-15 19:03:53 -0700

    guestion 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include<stdio.h>
+/*
+ * main : print the string in the main function
+ * description : use of main function
+ * return o
+ */
+int main(void)
+{
+	printf(" with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
